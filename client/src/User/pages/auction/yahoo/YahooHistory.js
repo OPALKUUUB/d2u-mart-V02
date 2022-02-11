96149d8decdd763e3f82718f52e71c5b6b7c08b6
Makara Atipatipat <makara.atipat@gmail.com>
2022-02-11 08:33:23 +0700

    add yahoo tracking

--- client/src/User/pages/auction/yahoo/YahooHistory.js
+++ client/src/User/pages/auction/yahoo/YahooHistory.js
@@ -65,6 +65,21 @@ export default function YahooHistory() {
                   ? "-"
                   : "ชำระเงินเรียบร้อยแล้ว"}
               </td>
+              <td className="align-middle">{item.track_id}</td>
+              <td className="align-middle">{item.box_id}</td>
+              <td className="align-middle">{item.weight}</td>
+              <td className="align-middle">
+                {item.round_boat === null || item.round_boat === "" ? (
+                  ""
+                ) : (
+                  <>
+                    {parseInt(item.round_boat.split("-")[2])}{" "}
+                    {month[parseInt(item.round_boat.split("-")[1])]}
+                  </>
+                )}
+                {/* {parseInt(item.round_boat.split("-")[2])}{" "}
+              {month[parseInt(item.round_boat.split("-")[1])]} */}
+              </td>
             </tr>
           ))}
         </tbody>
@@ -72,3 +87,18 @@ export default function YahooHistory() {
     </>
   );
 }
+
+const month = {
+  1: "JAN",
+  2: "FEB",
+  3: "MAR",
+  4: "APR",
+  5: "MAY",
+  6: "JUN",
+  7: "JUL",
+  8: "AUG",
+  9: "SEPT",
+  10: "OCT",
+  11: "NOV",
+  12: "DEC",
+};
