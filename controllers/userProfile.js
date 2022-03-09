8ed2ca22928a457b90a423a09f50109da556d605
Makara Atipatipat <makara.atipat@gmail.com>
2022-03-09 11:34:10 +0700

    test

--- controllers/userProfile.js
+++ controllers/userProfile.js
@@ -1,6 +1,17 @@
 const conn = require("../configs/connection");
 const jwt = require("jsonwebtoken");
 
+function genDate() {
+  let today = new Date();
+  let date = today.getDate() >= 10 ? today.getDate() : `0${today.getDate()}`;
+  let month =
+    today.getMonth() >= 10 ? today.getMonth() + 1 : `0${today.getMonth() + 1}`;
+  let hour = today.getHours() >= 10 ? today.getHours() : `0${today.getHours()}`;
+  let minute =
+    today.getMinutes() >= 10 ? today.getMinutes() : `0${today.getMinutes()}`;
+  return `${today.getFullYear()}-${month}-${date}T${hour}:${minute}`;
+}
+
 function jwtVerify(token) {
   let decoded = jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
     if (error) {
