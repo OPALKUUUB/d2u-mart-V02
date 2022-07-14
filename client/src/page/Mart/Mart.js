3703cbbade0db75bfaf073f384ec05768078407a
whi7ent <wippstkk@gmail.com>
2022-07-14 23:29:38 +0700

    test

--- client/src/page/Mart/Mart.js
+++ client/src/page/Mart/Mart.js
@@ -1,13 +1,26 @@
 import React from "react";
-import Card from "../../component/Card/Card";
+import { useNavigate } from "react-router-dom";
+import "./Mart.css";
 
 const Mart = () => {
+  const navigate = useNavigate()
   return (
-    <section style={{ marginTop: "50px" }}>
-      <Card text="ร้านค้า" link="/mart/shop" />
-      <Card text="โปรโมชั่น" link="/mart/shop/showmorepromotion" />
-      <Card text="หมวดหมู่" />
-    </section>
+      <div className="center">
+        <div className="card-container">
+            <h2 className="cardShop">ร้านค้า</h2>
+            <span className="dot"></span>
+            <span className="learnmore">เรียนรู้เพิ่มเติม</span>
+            <p className="verti-text">SHOP</p>
+            <img className="bgimg" src="/image/productCard.png" alt="" onClick={()=>navigate("/mart/shop")}></img>
+        </div>
+        <div className="card-container">
+            <h2 className="cardShop2">โปรโมชั่น</h2>
+            <span className="dot2"></span>
+            <span className="learnmore2">เรียนรู้เพิ่มเติม</span>
+            <p className="verti-text2">PROMO</p>
+            <img className="bgimg" src="/image/promotionCard.png" alt="" onClick={()=>navigate("/mart/shop/showmorepromotion")}></img>
+        </div>
+      </div>
   );
 };
 
