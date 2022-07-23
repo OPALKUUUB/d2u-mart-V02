abb5e77ab713c6afba09b87449b20c7203951f16
OPALKUUUB <makara.atipat@gmail.com>
2022-07-23 22:00:12 +0700

    fix styled in subCard to 5 col(lg), 3 col(md), 2 col(sm) and add include tax and expire_date

--- client/src/page/Mart/Shop/ShowMore/ShowMoreAllItem.js
+++ client/src/page/Mart/Shop/ShowMore/ShowMoreAllItem.js
@@ -1,6 +1,6 @@
 import BackButt from "../../../../component/button/BackButt";
 import SubCard from "../../../../component/SubCard/SubCard";
-import "../../Martshop/MartMenu.css"
+import "../../Martshop/MartMenu.css";
 import { useEffect, useRef, useState } from "react";
 import { getAllCategory, getItemInCategory } from "../api";
 import { useParams } from "react-router-dom";
@@ -10,75 +10,99 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./ShowMoreAllItem.css";
-import { Navigation , Pagination } from "swiper";
+import { Navigation, Pagination } from "swiper";
 import Basket from "../../../../component/Basket/Basket";
 const ShowMoreAllItem = () => {
-    const [ allCategory , setAllCategory ] = useState([])
-    const [ categorySelected , setCategorySelected ] = useState(0);
-    const [ allItemData , setAllItemData ] = useState([]);
-    const [ currentPage , setCurrentPage ] = useState(0);
-    const [ numberOfPage , setNumberOfPage ] = useState(0);
-    const [ pageIndexList , setPageIndexList ] = useState([]);
-    const params = useParams();
-    function firebaseCollectionConditionByShop(){
-        if(params.shop === 'daiso'){
-            return 'daisonet.com';
-        }
-        if(params.shop === 'abc'){
-            return 'www.abc-mart.net'
-        }
+  const [allCategory, setAllCategory] = useState([]);
+  const [categorySelected, setCategorySelected] = useState(0);
+  const [allItemData, setAllItemData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [numberOfPage, setNumberOfPage] = useState(0);
+  const [pageIndexList, setPageIndexList] = useState([]);
+  const params = useParams();
+  function firebaseCollectionConditionByShop() {
+    if (params.shop === "daiso") {
+      return "daisonet.com";
     }
+    if (params.shop === "abc") {
+      return "www.abc-mart.net";
+    }
+  }
 
-    useEffect(()=>{
-        window.scrollTo(0, 0);
-        (async function(){
-            let resultCategory = await getAllCategory(firebaseCollectionConditionByShop());
-            let categoryList = [];
-            resultCategory.forEach((category)=>{
-                categoryList.push(category.data())
-            })
-            setAllCategory(categoryList);
-        }())
-    },[])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    (async function () {
+      let resultCategory = await getAllCategory(
+        firebaseCollectionConditionByShop()
+      );
+      let categoryList = [];
+      resultCategory.forEach((category) => {
+        categoryList.push(category.data());
+      });
+      setAllCategory(categoryList);
+    })();
+  }, []);
 
-    useEffect(()=>{
-        if(allCategory.length > 0){
-            (async function(){
-                let resultItems = await getItemInCategory(firebaseCollectionConditionByShop(),allCategory[categorySelected]?.name);
-                setAllItemData(resultItems);
-                setNumberOfPage(Math.ceil(resultItems.length / 18));
-                setCurrentPage(0);
-                let indexList = [];
-                for(let i = 0 ; i < Math.ceil(resultItems.length / 18) ; i++){
-                    indexList.push(i+1);
-                }
-                setPageIndexList(indexList);
-            }());
+  useEffect(() => {
+    if (allCategory.length > 0) {
+      (async function () {
+        let resultItems = await getItemInCategory(
+          firebaseCollectionConditionByShop(),
+          allCategory[categorySelected]?.name
+        );
+        setAllItemData(resultItems);
+        setNumberOfPage(Math.ceil(resultItems.length / 18));
+        setCurrentPage(0);
+        let indexList = [];
+        for (let i = 0; i < Math.ceil(resultItems.length / 18); i++) {
+          indexList.push(i + 1);
         }
-    },[allCategory , categorySelected])
+        setPageIndexList(indexList);
+      })();
+    }
+  }, [allCategory, categorySelected]);
 
-    return (
-        <section style={{backgroundColor:'#e6e5e1' , fontFamily: '"Prompt", sans-serif'}}>
-            <BackButt link ="/mart/shop" />
-            <img src="/image/daisoCover.png" className="cover" alt=""></img>
-            <ProductCategory key={'productCategory'} allCategory={allCategory} categorySelected={categorySelected} setCategorySelected={setCategorySelected}/>
-            <div id="allProduct" className="w-full flex flex-col bg-[#e6e5e1] items-center py-24 gap-12">
-                <div className="w-full max-w-[1100px] flex flex-col items-center gap-8">
-                    <div className="w-full flex justify-start items-end gap-2 pl-[10px]">
-                        <p className="m-0 text-[30px]">สินค้า</p>
-                        <p className="m-0 text-[26px] text-[#f0a28e]">ทั้งหมด</p>
-                    </div>
-                    <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-x-8 gap-y-3">
-                        {allItemData.map((item,index)=>{
-                            if(index >= currentPage * 18 && index < (currentPage+1)*18){
-                                return <SubCard key={`subcard${index}`} text = {item?.name} image_url = {item?.image_url} price={item?.price} item={item} />
-                            }
-                        })}
-                    </div>
-                </div>
-                <div className="w-full flex justify-center items-start max-w-[550px] gap-[10px] py-1 relative">
-                    <p className="m-0 text-[22px] pt-2">หน้า</p>
-                    {/* <div className={`flex text-[20px] pt-[10px] cursor-pointer ease-linear duration-200 ${currentPage !== 0 ? 'text-[#7b7e7c]' : 'text-[#7e817f39]'}`}
+  return (
+    <section
+      style={{ backgroundColor: "#e6e5e1", fontFamily: '"Prompt", sans-serif' }}
+    >
+      <BackButt link="/mart/shop" />
+      <img src="/image/daisoCover.png" className="cover" alt=""></img>
+      <ProductCategory
+        key={"productCategory"}
+        allCategory={allCategory}
+        categorySelected={categorySelected}
+        setCategorySelected={setCategorySelected}
+      />
+      <div
+        id="allProduct"
+        className="w-full flex flex-col bg-[#e6e5e1] items-center py-24 gap-12"
+      >
+        <div className="w-full max-w-[1100px] flex flex-col items-center gap-8">
+          <div className="w-full flex justify-start items-end gap-2 pl-[10px]">
+            <p className="m-0 text-[30px]">สินค้า</p>
+            <p className="m-0 text-[26px] text-[#f0a28e]">ทั้งหมด</p>
+          </div>
+          <div className="w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 justify-items-center gap-x-1 gap-y-3">
+            {allItemData.map((item, index) => {
+              if (index >= currentPage * 18 && index < (currentPage + 1) * 18) {
+                return (
+                  <SubCard
+                    key={`subcard${index}`}
+                    text={item?.name}
+                    image_url={item?.image_url}
+                    price={item?.price}
+                    item={item}
+                    expire_date={item?.expire_date}
+                  />
+                );
+              }
+            })}
+          </div>
+        </div>
+        <div className="w-full flex justify-center items-start max-w-[550px] gap-[10px] py-1 relative">
+          <p className="m-0 text-[22px] pt-2">หน้า</p>
+          {/* <div className={`flex text-[20px] pt-[10px] cursor-pointer ease-linear duration-200 ${currentPage !== 0 ? 'text-[#7b7e7c]' : 'text-[#7e817f39]'}`}
                         onClick={()=>{
                             if(currentPage !== 0) {
                                 // if(currentPage % 10 === 1){
@@ -90,48 +114,52 @@ const ShowMoreAllItem = () => {
                             }
                         }}
                     >{'<<'}</div> */}
-                    <div className="flex w-full justify-center"
-                        style={{maxWidth:`${numberOfPage*550/10}px`}}
+          <div
+            className="flex w-full justify-center"
+            style={{ maxWidth: `${(numberOfPage * 550) / 10}px` }}
+          >
+            {pageIndexList && (
+              <Swiper
+                navigation={true}
+                modules={[Navigation, Pagination]}
+                pagination={{
+                  clickable: true,
+                }}
+                slidesPerView={numberOfPage >= 10 ? 10 : numberOfPage}
+                slidesPerGroup={10}
+                spaceBetween={8}
+                className=" w-full flex justify-center justify-items-center h-[100px] relative px-1 showmore py-1"
+              >
+                {pageIndexList?.map((page, index) => (
+                  <SwiperSlide key={`pageIndexList${index}`}>
+                    <div
+                      className={`cursor-pointer p-2 rounded-xl text-gray-500 border-gray-300 border-[2px] flex items-center justify-center hover:scale-110 ease-linear duration-150 ${
+                        currentPage === index && "bg-[#c8ceca]"
+                      }`}
+                      onClick={() => {
+                        setCurrentPage(index);
+                        window.location.href = "#allProduct";
+                      }}
                     >
-                        {pageIndexList && 
-                        <Swiper 
-                            navigation={true}
-                            modules={[Navigation , Pagination]}
-                            pagination={{
-                                clickable: true,
-                            }}
-                            slidesPerView={numberOfPage >= 10 ? 10 : numberOfPage }
-                            slidesPerGroup={10}
-                            spaceBetween={8}
-                            className=" w-full flex justify-center justify-items-center h-[100px] relative px-1 showmore py-1"
-                        >
-                            {pageIndexList?.map((page , index)=>(
-                                <SwiperSlide key={`pageIndexList${index}`}>
-                                    <div className={`cursor-pointer p-2 rounded-xl text-gray-500 border-gray-300 border-[2px] flex items-center justify-center hover:scale-110 ease-linear duration-150 ${currentPage === index && 'bg-[#c8ceca]'}`}
-                                        onClick={()=>{
-                                            setCurrentPage(index);
-                                            window.location.href = "#allProduct";
-                                        }}
-                                    >
-                                        <p className="m-0 text-[16px] text-center">{page}</p>
-                                    </div>
-                                    
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>}
+                      <p className="m-0 text-[16px] text-center">{page}</p>
                     </div>
-                    {/* <div className={`flex text-[20px] text-[#7b7e7c] pt-[10px] cursor-pointer ease-linear duration-200 ${currentPage+1 !== numberOfPage ? 'text-[#7b7e7c]' : 'text-[#7e817f39]'} `}
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            )}
+          </div>
+          {/* <div className={`flex text-[20px] text-[#7b7e7c] pt-[10px] cursor-pointer ease-linear duration-200 ${currentPage+1 !== numberOfPage ? 'text-[#7b7e7c]' : 'text-[#7e817f39]'} `}
                         onClick={()=>{
                             if(currentPage+1 !== numberOfPage){
                                 setCurrentPage(prev => prev+1);
                             }
                         }}
                     >{'>>'}</div> */}
-                </div>
-            </div>
-            <Basket/>
-        </section>
-      );
-  };
-  
+        </div>
+      </div>
+      <Basket />
+    </section>
+  );
+};
+
 export default ShowMoreAllItem;
