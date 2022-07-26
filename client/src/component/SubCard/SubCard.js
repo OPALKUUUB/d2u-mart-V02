33031b6c5d5b9b72333a066319ffe159ba7d5df0
OPALKUUUB <makara.atipat@gmail.com>
2022-07-26 21:38:24 +0700

    test

--- client/src/component/SubCard/SubCard.js
+++ client/src/component/SubCard/SubCard.js
@@ -8,116 +8,70 @@ const SubCard = (props) => {
   const navigate = useNavigate();
   const [itemInBasket, setItemInBasket] = useRecoilState(basketState);
   return (
-    <>
-      <div
-        className="SubCard-container"
-        style={{ fontFamily: '"Prompt", sans-serif' }}
-      >
-        <div className="SubCard-card">
-          <div className="SubCard-imgBx">
-            <img
-              className="w-[180px] h-[180px] object-cover object-center rounded-lg"
-              alt=""
-              src={
-                props.image_url !== undefined
-                  ? props.image_url
-                  : "https://www.daisonet.com/images/common/header/logo_pc.jpg"
-              }
-            />
-          </div>
-          <div className="SubCard-txt">
-            <h2 className="text-[14px]">{props.text}</h2>
-            <p className="mb-0 text-[15px] font-semibold py-[2px]">
-              {props.price}
-            </p>
-          </div>
-          <div className="SubCard-contentBx">
-            <div
-              className="flex"
-              onClick={() => {
-                if (props.link !== undefined) {
-                  navigate(props.link);
-                } else {
-                  if (
-                    itemInBasket.findIndex(
-                      (itemInBasket) => itemInBasket?.id === props?.item.id
-                    ) !== -1
-                  ) {
-                    setItemInBasket(
-                      itemInBasket.map((itemInBasket) => {
-                        if (itemInBasket?.id === props?.item.id) {
-                          return {
-                            ...itemInBasket,
-                            count: itemInBasket.count + 1,
-                          };
-                        } else {
-                          return itemInBasket;
-                        }
-                      })
-                    );
-                  } else {
-                    let itemToBasket = {
-                      ...props.item,
-                      count: 1,
-                    };
-                    setItemInBasket([...itemInBasket, itemToBasket]);
-                  }
-                }
-              }}
-            >
-              {props.link === undefined && <AddShoppingCartIcon />}
-              {props.link === undefined ? "เพิ่มลงตะกร้า" : "ซื้อเลย"}
-            </div>
-            <h3>exp.</h3>
-            <h3>tax</h3>
-          </div>
-        </div>
-      </div>
-      <div className="SubCard-txt">
-        <h2 className="text-[18px]">{props.text}</h2>
-        <p className="mb-0 text-[22px] font-semibold py-[2px]">{props.price}</p>
-      </div>
-      <div className="SubCard-contentBx">
-        <div
-          className="flex"
-          onClick={() => {
-            if (props.link !== undefined) {
-              navigate(props.link);
-            } else {
-              if (
-                itemInBasket.findIndex(
-                  (itemInBasket) => itemInBasket?.id === props?.item.id
-                ) !== -1
-              ) {
-                setItemInBasket(
-                  itemInBasket.map((itemInBasket) => {
-                    if (itemInBasket?.id === props?.item.id) {
-                      return {
-                        ...itemInBasket,
-                        count: itemInBasket.count + 1,
-                      };
-                    } else {
-                      return itemInBasket;
-                    }
-                  })
-                );
-              } else {
-                let itemToBasket = {
-                  ...props.item,
-                  count: 1,
-                };
-                setItemInBasket([...itemInBasket, itemToBasket]);
-              }
+    <div
+      className="SubCard-container"
+      style={{ fontFamily: '"Prompt", sans-serif' }}
+    >
+      <div className="SubCard-card">
+        <div className="SubCard-imgBx">
+          <img
+            className="w-[180px] h-[180px] object-cover object-center rounded-lg"
+            alt=""
+            src={
+              props.image_url !== undefined
+                ? props.image_url
+                : "https://www.daisonet.com/images/common/header/logo_pc.jpg"
             }
-          }}
-        >
-          {props.link === undefined && <AddShoppingCartIcon />}
-          {props.link === undefined ? "เพิ่มลงตะกร้า" : "ซื้อเลย"}
+          />
         </div>
-        <h3>exp.</h3>
-        <h3>tax</h3>
+        <div className="SubCard-txt">
+          <h2 className="text-[14px]">{props.text}</h2>
+          <p className="mb-0 text-[15px] font-semibold py-[2px]">
+            {props.price}
+          </p>
+        </div>
+        <div className="SubCard-contentBx">
+          <div
+            className="flex"
+            onClick={() => {
+              if (props.link !== undefined) {
+                navigate(props.link);
+              } else {
+                if (
+                  itemInBasket.findIndex(
+                    (itemInBasket) => itemInBasket?.id === props?.item.id
+                  ) !== -1
+                ) {
+                  setItemInBasket(
+                    itemInBasket.map((itemInBasket) => {
+                      if (itemInBasket?.id === props?.item.id) {
+                        return {
+                          ...itemInBasket,
+                          count: itemInBasket.count + 1,
+                        };
+                      } else {
+                        return itemInBasket;
+                      }
+                    })
+                  );
+                } else {
+                  let itemToBasket = {
+                    ...props.item,
+                    count: 1,
+                  };
+                  setItemInBasket([...itemInBasket, itemToBasket]);
+                }
+              }
+            }}
+          >
+            {props.link === undefined && <AddShoppingCartIcon />}
+            {props.link === undefined ? "เพิ่มลงตะกร้า" : "ซื้อเลย"}
+          </div>
+          <h3>exp.</h3>
+          <h3>tax</h3>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
