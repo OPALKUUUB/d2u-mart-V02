74007cfcc42c219ee0f048f92e3c23822327bbb6
OPALKUUUB <makara.atipat@gmail.com>
2022-07-26 21:44:20 +0700

    test

--- client/src/page/OurService/OurService.js
+++ client/src/page/OurService/OurService.js
@@ -3,7 +3,7 @@ import React from "react";
 export const OurService = () => {
   return (
     <>
-      <div className="">
+      <div className="flex flex-col">
         <img src="/image/service_us.jpg" />
         <img src="/image/service_us_2.jpg" />
         <img src="/image/service_us_3.jpg" />
