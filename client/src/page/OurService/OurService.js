a002801a881fb1bb6ee9eeb02a1338280fc34624
OPALKUUUB <makara.atipat@gmail.com>
2022-08-28 15:00:15 +0700

    test

--- client/src/page/OurService/OurService.js
+++ client/src/page/OurService/OurService.js
@@ -1,14 +1,25 @@
 import React from "react";
+import "./OurService.css";
 
 export const OurService = () => {
   return (
     <>
-      <div className="flex flex-col">
-        <img src="/image/service_us.jpg" />
-        <img src="/image/service_us_2.jpg" />
-        <img src="/image/service_us_3.jpg" />
-        <img src="/image/service_us_4.jpg" />
-        <img src="/image/service_us_5.jpg" />
+      <div className="OurService">
+        <div className="bg">
+          <img src="/image/bg-wood.jpg" />
+          <img src="/image/bg-wood.jpg" />
+          <img src="/image/bg-wood.jpg" />
+          <img src="/image/bg-wood.jpg" />
+          <img src="/image/bg-wood.jpg" />
+          <img src="/image/bg-wood.jpg" />
+        </div>
+        <div className="content">
+          <img src="/image/service_us.jpg" />
+          <img src="/image/service_us_2.jpg" />
+          <img src="/image/service_us_3.jpg" />
+          <img src="/image/service_us_4.jpg" />
+          <img src="/image/service_us_5.jpg" />
+        </div>
       </div>
     </>
   );
