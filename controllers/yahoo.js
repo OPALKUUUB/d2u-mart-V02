29e7fac2823e113bc2e8b03b338994c57c59232a
OPALKUUUB <makara.atipat@gmail.com>
2022-10-07 07:56:24 +0700

    test

--- controllers/yahoo.js
+++ controllers/yahoo.js
@@ -10,7 +10,7 @@ function genDate() {
   let today = new Date();
   let date = today.getDate() >= 10 ? today.getDate() : `0${today.getDate()}`;
   let month =
-    today.getMonth() >= 10 ? today.getMonth() + 1 : `0${today.getMonth() + 1}`;
+    today.getMonth() >= 9 ? today.getMonth() + 1 : `0${today.getMonth() + 1}`;
   let hour = today.getHours() >= 10 ? today.getHours() : `0${today.getHours()}`;
   let minute =
     today.getMinutes() >= 10 ? today.getMinutes() : `0${today.getMinutes()}`;
